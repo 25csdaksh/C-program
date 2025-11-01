d4b01c0877ca7dbb438a73268d57af895980d360
Daksh Soni <25cs102@charusat.edu.in>
2025-11-01 12:16:04 +0530

    first commit

---
+++ project.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()  
+{
+    int l, b;
+    printf("\nEnter the value of l =");
+    scanf("%d", &l);
+
+    printf("\nEnter the value of b =");
+    scanf("%d", &b);
+
+    printf("The area of rectangle is : %d", l * b);
+    return 0;
+}
